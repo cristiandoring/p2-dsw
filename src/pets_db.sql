e3a37bd29aae88dfe6d1f6df0a22d0f0f8da863f
Cristian Doring <cristiandoring@gmail.com>
2026-06-23 22:20:04 -0300

    ajuste no database para aceitar valores nulos no role de usuarios

--- src/pets_db.sql
+++ src/pets_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    role VARCHAR(20) NOT NULL
+    role VARCHAR(20)
 );
 
 CREATE TABLE pets (
